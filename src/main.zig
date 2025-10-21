1979655dca2646310aec7cd767a4901ac93407c8
qbornet <qbornet@proton.me>
2025-10-21 14:52:14 +0200

    [WIP] Modified main first step.

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,5 @@ const std = @import("std");
 const optimize = @import("builtin").mode;
 
 pub fn main() !void {
-    switch (optimize) {
-        .Debug => std.debug.print("You are in debug mode: '{s}'\n", .{@tagName(optimize)}),
-        else => std.debug.print("You are in {s} mode.\n", .{@tagName(optimize)}),
-    }
+    std.debug.print("Optimization mode: '{s}'\n", .{@tagName(optimize)});
 }
